bad738c0c02968b9d9a475ec77a3cfd70a88604f
patricio97m <patricio.97m@gmail.com>
2022-12-22 21:39:36 -0300

    pagina terminada

---
+++ js/script.js
@@ -0,0 +1,42 @@
+const form = document.getElementById("newForm")
+const nombre = document.getElementById("name");
+const email = document.getElementById("email");
+const comentario = document.getElementById("comentarios");
+let regexNombre = /^[a-zA-Z]+$/;
+let regexEmail = /^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[0-9a-zA-Z._.-]+$/;
+
+
+form.addEventListener("click", (e) => {
+    validarConsulta()
+});
+
+function reset() {
+    nombre.value = "";
+    email.value = "";
+    comentario.value = "";
+}
+
+function submit() {
+    let newQuery = {
+        nombre: nombre.value,
+        email: email.value,
+        comentario: comentario.value
+    }
+    localStorage.setItem("newQuery", JSON.stringify(newQuery));
+    alert("Consulta enviada con éxito")
+    reset();
+}
+function validarConsulta(){
+    let error = false;
+    if (!regexNombre.test(nombre.value)) {
+        error = true;
+        alert("Nombre inválido");
+    }
+    if (!regexEmail.test(email.value)) {
+        error = true;
+        alert("Email inválido");
+    }
+    else{
+        submit();
+    }
+}
